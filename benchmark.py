bd4d698b074a84e7b448b0363727dcd7e47a2e5f
Jaewon Lee <amshoo82@gmail.com>
2023-05-02 14:21:30 -0400

    add accel mode to benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,53 +1,68 @@
 class Benchmark:
-    def __init__(self,mode):
+    def __init__(self, mode):
         if mode == "macsim":
-            self.benchmark_list= [
-            #"backprop",
-            "bfs",
-            "cfd",
-            "hotspot",
-            "kmeans",
-            #"lavaMD", C extern is used with template
-            #"lud", #seg fault but we can get result
-            #"nw",
-            #"srad",
-            "streamcluster",
-            #"pathfinder",
-            #"particlefilter",
-            #"gaussian",
-            "nn",
-            #"heartwall",
-            "hybridsort",  #need
-            #"b+tree",NOOO
-            #"dwt2d", NOOO
-            #"hotspot3D", NOOO
-            #"leukocyte", NOOO
-            #"myocyte", NOOO
+            self.benchmark_list = [
+                # "backprop",
+                "bfs",
+                "cfd",
+                "hotspot",
+                "kmeans",
+                # "lavaMD", C extern is used with template
+                # "lud", #seg fault but we can get result
+                # "nw",
+                # "srad",
+                "streamcluster",
+                # "pathfinder",
+                # "particlefilter",
+                # "gaussian",
+                "nn",
+                # "heartwall",
+                "hybridsort",  # need
+                # "b+tree",NOOO
+                #"dwt2d", NOOO
+                #"hotspot3D", NOOO
+                #"leukocyte", NOOO
+                #"myocyte", NOOO
             ]
         elif mode == "igpu":
-            self.benchmark_list= [
-            "backprop",
-            "bfs",
-            "cfd",
-            "hotspot",
-            "kmeans",
-            #"lavaMD", C extern is used with template
-            "lud", #seg fault but we can get result
-            "nw",
-            "srad",
-            #"streamcluster",
-            "pathfinder",
-            #"particlefilter",
-            "gaussian",
-            "nn",
-            "heartwall",
-            "hybridsort",  #need
-            #"b+tree",NOOO
-            #"dwt2d", NOOO
-            #"hotspot3D", NOOO
-            #"leukocyte", NOOO
-            #"myocyte", NOOO
+            self.benchmark_list = [
+                "backprop",
+                "bfs",
+                "cfd",
+                "hotspot",
+                "kmeans",
+                # "lavaMD", C extern is used with template
+                "lud",  # seg fault but we can get result
+                "nw",
+                "srad",
+                # "streamcluster",
+                "pathfinder",
+                # "particlefilter",
+                "gaussian",
+                "nn",
+                "heartwall",
+                "hybridsort",  # need
+                # "b+tree",NOOO
+                #"dwt2d", NOOO
+                #"hotspot3D", NOOO
+                #"leukocyte", NOOO
+                #"myocyte", NOOO
             ]
+        elif mode == "ptx":
+            self.benchmark_list = [
+                "ScalarProd",
+                "SobolQRNG",
+                "Histogram",
+                "Dxtc",
+                "sssp",
+                "streamcluster",  # seg fault but we can get result
+                "lud-64",
+                "bfs-dtc",
+                "MergeSort",
+            ]
+        elif mode == "accel":
+            self.benchmark_list = [
+                "backprop", "bfs", "b_tree", "dwt2d", "gausian", "hotspot", "lud", "particle_filter", "pathfinder"]
         else:
             print("ERR: select macsim or igpu")
             exit()
@@ -55,5 +70,5 @@ class Benchmark:
     def get_benchmark_list(self):
         return self.benchmark_list
 
-    def get_benchmark_index(self,name):
+    def get_benchmark_index(self, name):
         return self.benchmark_list.index(name)
